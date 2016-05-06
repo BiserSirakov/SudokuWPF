bf9da63658db2df46c6b27489d28c8e6720bf75d
BiserSirakov <biser.sirakov@gmail.com>
2016-05-06 17:17:43 +0300

    Separated in folders (VMMV pattern)

---
+++ SudokuWPF/SudokuWPF/View/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace SudokuWPF.View
+{
+    using System.Windows;
+
+    using ViewModel;
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModelClass _viewModel;
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
